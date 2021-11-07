b9b225ef49c184bcb3f8e44ad8ff137bc9c06924
Joseph John <jjohn2@tcs.woolworths.com.au>
2021-11-07 23:51:27 +1100

    dev branch

--- src/main/java/com/inapp/SpringActionApplication.java
+++ src/main/java/com/inapp/SpringActionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringActionApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Main method..");
 		SpringApplication.run(SpringActionApplication.class, args);
 	}
 
